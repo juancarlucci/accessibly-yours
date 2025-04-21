5d8a1a16f0f04f0a0515b8836e158ec855b58fff
juancarlucci <juancarloscollins@gmail.com>
2025-04-21 22:17:28 +0200

    fix: update Tailwind config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "export", // ⬅️ Required for static export in Next.js 14+
   basePath: "/accessibly-yours", // ⬅️ Your GitHub repo name
-  assetPrefix: "/accessibly-yours", // ⬅️ Ensures static files load properly
+  assetPrefix: "/accessibly-yours/", // ⬅️ Ensures static files load properly
 };
 
 export default nextConfig;
