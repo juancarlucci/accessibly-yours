1388e64c9a563518bc8f8a7c60ea06b310467200
juancarlucci <juancarloscollins@gmail.com>
2025-04-21 18:49:24 +0200

    feat (page.tsx): add UrlScanner component to Home page for enhanced accessibility scanning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+//* src/app/page.tsx
 import HeroSection from "../components/HeroSection";
 import FeatureCards from "../components/FeatureCards";
+import UrlScanner from "../components/UrlScanner";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <FeatureCards />
+      <UrlScanner />
     </>
   );
 }
