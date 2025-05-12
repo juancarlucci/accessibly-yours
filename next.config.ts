8c2d10911723edca8e83df1a9cbe6c46bc31bec8
juancarlucci <juancarloscollins@gmail.com>
2025-05-12 12:37:10 -0700

    chore: update dependencies and add new assets
    - Added clsx and tailwind-variants to dependencies in package.json
    - Added logo.png to the public directory
    - Updated favicon.ico in the app directory

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,45 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compress: true, // ✅ Enable gzip compression
-  swcMinify: true, // ✅ Use super-fast SWC minifier
-  experimental: {
-    serverActions: true, // ✅ Future-proofing (optional, safe to leave)
+  compress: true,
+  webpack: (
+    config: {
+      optimization: {
+        splitChunks: {
+          chunks: string;
+          minSize: number;
+          maxSize: number;
+          cacheGroups: {
+            vendor: {
+              test: RegExp;
+              name(module: {
+                context: { match: (arg0: RegExp) => unknown[] };
+              }): string;
+            };
+          };
+        };
+      };
+    },
+  ) => {
+    // Enable granular chunks
+    config.optimization.splitChunks = {
+      chunks: "all",
+      minSize: 20000,
+      maxSize: 70000,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module: { context: { match: (arg0: RegExp) => unknown[] } }) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+            return `vendor.${(packageName as string).replace("@", "")}`;
+          },
+        },
+      },
+    };
+    return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
